28816cc92461d9f3f6699216c8cc04301c7dd702
Roushan <sugoi-roushansourav@github.com>
2021-07-01 19:25:44 +0530

    added fontfaceobserver-for-Open Sans

--- src/index.js
+++ src/index.js
@@ -2,32 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { ConnectedRouter } from "connected-react-router";
-import { Router } from "react-router-dom";
-// import FontFaceObserver from "fontfaceobserver";
+import FontFaceObserver from "fontfaceobserver";
 import history from "./utils/history";
 import "./index.css";
 import App from "./containers/App";
 import configureStore from "./configureStore";
 import reportWebVitals from "./reportWebVitals";
 
-// const openSansObserver = new FontFaceObserver("Open Sans", {});
+const openSansObserver = new FontFaceObserver("Open Sans", {});
 
 // Create redux store with history
 const initialState = {};
 const store = configureStore(initialState, history);
 
-// openSansObserver.load().then(() => {
-//   document.body.classList.add("fontLoaded");
-// });
+openSansObserver.load().then(() => {
+  document.body.classList.add("fontLoaded");
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <ConnectedRouter history={history}> */}
-      <Router history={history}>
+      <ConnectedRouter history={history}>
         <App />
-        {/* </ConnectedRouter> */}
-      </Router>
+      </ConnectedRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
@@ -37,12 +34,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-// Load the favicon and the .htaccess file
-// import '!file-loader?name=[name].[ext]!./images/favicon.ico';
-// import 'file-loader?name=.htaccess!./.htaccess'; // eslint-disable-line import/extensions
-
-// Observe loading of Open Sans (to remove open sans, remove the <link> tag in
-// the index.html file and this observer)
-
-// When Open Sans is loaded, add a font-family using Open Sans to the body
